9c786afc11ffcddf7f8ca11a75405ee3b0bd52ac
Prajakta Jangle <prajakta.jangle@accesshq.com>
2023-07-14 10:32:20 +1000

    added language setting

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Vault, Modal, Notice, Plugin, TFile, moment, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -17,12 +17,22 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('cat', 'Language Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Hi Praj!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+
+		// This creates an icon in the left ribbon.
+		const ribbonIconElFile = this.addRibbonIcon('chevron-right', 'Custom daily note', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			// filename = new FileSystemAdapter().getName(); 
+			new Notice(this.app.vault.());
+		});
+
+		ribbonIconElFile.addClass('my-custom-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
@@ -38,11 +48,12 @@ export default class MyPlugin extends Plugin {
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'daily-note-in-custom-language',
+			name: 'Daily note in custom Language',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				moment.locale(DEFAULT_SETTINGS.mySetting);
+				new Notice(moment.locales());
+				editor.replaceSelection(moment().format("dddd, MMMM Do"));
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
@@ -124,13 +135,13 @@ class SampleSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setDesc('Language choice')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('Enter your language of choice for daily notes')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
 					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					DEFAULT_SETTINGS.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
 	}
